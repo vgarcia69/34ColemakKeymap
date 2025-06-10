c80ff1d7a8e53cec3481b733b63a3a969b43f9bc
nedd06 <vincent.garcia6942@gmail.com>
2025-06-10 21:51:37 +0200

    .

--- keymap.c
+++ keymap.c
@@ -7,7 +7,7 @@
 // ============================================================================
 
 // Combo key sequences
-const uint16_t PROGMEM _st_combo[] = {KC_S, KC_T, COMBO_END};
+const uint16_t PROGMEM _st_combo[] = {LGUI_T(KC_S), LALT_T(KC_T), COMBO_END};
 const uint16_t PROGMEM _fp_combo[] = {KC_F, KC_P, COMBO_END};
 const uint16_t PROGMEM _lu_combo[] = {KC_L, KC_U, COMBO_END};
 
@@ -39,17 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌─────┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┐
      * │  Q  │  W  │  F  │  P  │  B  │       │  J  │  L  │  U  │  Y  │BSPC │
      * ├─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┤
-     * │  A  │  R  │  S  │  T  │  G  │       │  M  │  N  │  E  │  I  │  O  │
+     * │  A  │CTL/R│GUI/S│ALT/T│  G  │       │  M  │ALT/N│GUI/E│CTL/I│  O  │
      * ├─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┤
-     * │  Z  │CTL/X│GUI/C│ALT/D│  V  │       │  K  │ALT/H│GUI/,│CTL/.│  ;  │
+     * │  Z  │  X  │  C  │  D  │  V  │       │  K  │  H  │  ,  │  .  │  ;  │
      * └─────┴─────┴─────┼─────┼─────┤       ├─────┼─────┼─────┴─────┴─────┘
      *                   │ SPC │SHIFT│       │NUM  │ENTER|
      *                   └─────┴─────┘       └─────┴─────┘
      */
     [_BASE] = LAYOUT_split_3x5_3(
         KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,                  /**/        KC_J,         KC_L,         KC_U,            KC_Y,           KC_BSPC,
-        KC_A,         KC_R,         KC_S,         KC_T,         KC_G,                  /**/        KC_M,         KC_N,         KC_E,            KC_I,           KC_O,
-        KC_Z,         LCTL_T(KC_X), LGUI_T(KC_C), LALT_T(KC_D), KC_V,                  /**/        KC_K,         RALT_T(KC_H), RGUI_T(KC_COMM), RCTL_T(KC_DOT), KC_SCLN,
+        KC_A,         LCTL_T(KC_R), LGUI_T(KC_S), LALT_T(KC_T), KC_G,                  /**/        KC_M,         RALT_T(KC_N), RGUI_T(KC_E),    RCTL_T(KC_I),   KC_O,
+        KC_Z,         KC_X,         KC_C,         KC_D,         KC_V,                  /**/        KC_K,         KC_H,         KC_COMM,         KC_DOT,         KC_SCLN,
                                     XXX,          LT(_FUNCTION, KC_SPC), KC_LSFT,      /**/        TO(_MEDIA), KC_ENT,       XXX
     ),
 
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBERS] = LAYOUT_split_3x5_3(
         RSFT(KC_SLSH),KC_7,         KC_8,         KC_9,         KC_TILD,   /**/        KC_MPRV,      KC_MPLY,      KC_MNXT,      XXX,          KC_BSPC,
         KC_SLSH,      KC_4,         KC_5,         KC_6,         KC_GRV,    /**/        KC_MUTE,      KC_VOLD,      KC_VOLU,      XXX,          KC_DEL,
-        KC_0,         KC_1,         KC_2,         KC_3,         XXX,       /**/        XXX,          KC_RALT,      KC_RGUI,      KC_RCTL,      XXX,     
+        KC_0,         KC_1,         KC_2,         KC_3,         XXX,       /**/        XXX,          XXX,          XXX,          XXX,     
                                     XXX,          KC_TRNS,      TO(_BASE), /**/        TO(_BASE), KC_ENT,       XXX
     ),
 
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EXLM,      KC_AT,        KC_HASH,      KC_DLR,       KC_PERC,  /**/        KC_UNDS,      KC_PLUS,      KC_DQUO,      KC_PIPE,      KC_BSPC,
         KC_CIRC,      KC_AMPR,      KC_ASTR,      KC_LPRN,      KC_RPRN,  /**/        KC_MINS,      KC_EQL,       KC_QUOT,      KC_BSLS,      KC_DEL,
         XXX,          KC_LBRC,      KC_LCBR,      KC_RCBR,      KC_RBRC,  /**/        XXX,          KC_SLSH,      RSFT(KC_COMM),RSFT(KC_DOT), RSFT(KC_SCLN), 
-                                    XXX,          KC_TRNS,      TO(_BASE),/**/        TO(_NUMBERS),    KC_ENT,       XXX
+                                    XXX,          KC_TRNS,      TO(_BASE),/**/        TO(_NUMBERS), KC_ENT,    XXX
     ),
 
     /*
